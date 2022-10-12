e9873017c06469945f9d2342c3f209cdc58aaf45
MD IMTIYAZ <64704175+Imtiyaz-78@users.noreply.github.com>
2022-10-12 21:10:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,23 @@ import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-		super(props);
+    super(props);
+    
+    this.state = {flag:false}
 	};
 
+  clickHandler(){
+    this.setState({flag:true})
+    console.log("click");
+    
+  }
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+        { /* Do not remove this main div!! */ }
+        {this.state.flag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+</p>}
+        <button id="click" onClick={()=>this.clickHandler()} >click</button>
     		</div>
     	);
     }
@@ -17,4 +27,3 @@ class App extends Component {
 
 
 export default App;
-
